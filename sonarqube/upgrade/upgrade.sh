984602552409224d55fbefd8a95676784f5a267b
GERGES Joe <jgerges@murex.com>
2020-07-29 15:00:11 +0300

    sonarqube automated upgrade

--- sonarqube/upgrade/upgrade.sh
+++ sonarqube/upgrade/upgrade.sh
@@ -3,6 +3,7 @@
 team=$1
 upgrade_to=$2
 web_context=$3
+hostname=$4
 
 #removing ingress rules
 helm upgrade $team-sonarqube .. -f myvals.yaml --set ingress.enabled='false'
@@ -16,4 +17,4 @@ helm upgrade $team-sonarqube .. -f myvals.yaml --set replicaCount='0',ingress.en
 helm upgrade $team-sonarqube .. -f myvals.yaml --set replicaCount='1',ingress.enabled='true',sonarqube.image.tag="$upgrade_to"
 
 #call playbook that checks if the service is up and calls the upgrade api as soon as it is up
-ansible-playbook upgrade.yaml --extra-vars "web_context=$web_context"
+ansible-playbook upgrade.yaml --extra-vars "web_context=$web_context hostname=$hostname"
