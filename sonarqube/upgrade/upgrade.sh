c86fe50a92bd0db737b1a8cda94b4b9678c59623
GERGES Joe <jgerges@murex.com>
2020-08-04 13:38:01 +0300

    changes

--- sonarqube/upgrade/upgrade.sh
+++ sonarqube/upgrade/upgrade.sh
@@ -9,24 +9,19 @@ web_context=$6
 
 terraform init
 
+#removing ingress for backup
 terraform apply -var 'rancher2_token_key=token-vkq9d:wg8gtt4gbgtk7nzfhlj4gs87dn4w2hxhd9qmcb9fmnqkllgx57792r' -var "upgrade_version=$current_version" -var 'ingress_enabled=false' -var 'replica_count=1' -auto-approve
 
+#getting pod name
 POD=$(kubectl get pod --all-namespaces -l app=sonarqube -o jsonpath="{.items[0].metadata.name}")
 
-#removing ingress rules
-rancher app upgrade --values myvals.yaml --set ingress.enabled='false' $team-sonarqube $app_version
-#helm upgrade $team-sonarqube .. -f myvals.yaml --set ingress.enabled='false'
+#backing up
+kubectl -n sonarqube exec $POD -c sonardb -- bash -c "pg_dump -U sonar sonar > sonar-backup.sql"
 
-#exec in container and create dup
-POD=$(kubectl get pod --all-namespaces -l app=sonarqube -o jsonpath="{.items[0].metadata.name}")
+#starting upgrade
+terraform apply -var 'rancher2_token_key=token-vkq9d:wg8gtt4gbgtk7nzfhlj4gs87dn4w2hxhd9qmcb9fmnqkllgx57792r' -var "upgrade_version=$upgrade_to" -var 'ingress_enabled=false' -var 'replica_count=0' -auto-approve
 
-#scale down to 0 to avoid conflicts and add ingress rules again
-rancher app upgrade --values myvals.yaml --set ingress.enabled='false' --set replicaCount='0' --set sonarqube.image.tag=$upgrade_to $team-sonarqube $app_version
-#helm upgrade $team-sonarqube .. -f myvals.yaml --set replicaCount='0',ingress.enabled='false',sonarqube.image.tag="$upgrade_to"
+#enabling ingress
+terraform apply -var 'rancher2_token_key=token-vkq9d:wg8gtt4gbgtk7nzfhlj4gs87dn4w2hxhd9qmcb9fmnqkllgx57792r' -var "upgrade_version=$upgrade_to" -var 'ingress_enabled=true' -var 'replica_count=1' -auto-approve
 
-#scale back up and enable ingress
-rancher app upgrade --values myvals.yaml --set ingress.enabled='true' --set replicaCount='1' --set sonarqube.image.tag=$upgrade_to $team-sonarqube $app_version
-#helm upgrade $team-sonarqube .. -f myvals.yaml --set replicaCount='1',ingress.enabled='true',sonarqube.image.tag="$upgrade_to"
-
-#call playbook that checks if the service is up and calls the upgrade api as soon as it is up
 ansible-playbook upgrade.yaml --extra-vars "web_context=$web_context hostname=$hostname"
