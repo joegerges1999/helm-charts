2ce255e7e770f0688c75438686843442f80117be
GERGES Joe <Joe.GERGES@murex.com>
2020-08-10 14:05:34 +0200

    changes

--- sonarqube/upgrade/upgrade.sh
+++ sonarqube/upgrade/upgrade.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
 
 team=$1
-app_version=$2
-upgrade_to=$3
-cluster_id=$4
-project_id=$5
+upgrade_to=$2
+cluster_id=c-jpxcn
+project_id=p-zwxgj
 
 echo "Fetching metadata ..."
 HOSTNAME=$(kubectl get --all-namespaces ingress -l app=sonarqube,team=$team -o jsonpath="{.items[0].spec.rules[0].host}")
 WEBCONTEXT=$(kubectl get --all-namespaces ingress -l app=sonarqube,team=$team -o jsonpath="{.items[0].spec.rules[0].http.paths[0].path}")
 POD=$(kubectl get pod --all-namespaces -l app=sonarqube,team=$team -o jsonpath="{.items[0].metadata.name}")
-current_version=$(kubectl get --all-namespaces deployment -l app=sonarqube,team=$tean -o jsonpath="{.items[0].spec.template.spec.containers[1].image}")
+current_version=$(kubectl get --all-namespaces deployment -l app=sonarqube,team=$team -o jsonpath="{.items[0].spec.template.spec.containers[1].image}")
 current_version=$(cut -d ":" -f2 <<< "$current_version")
 
 echo "Logging in to rancher ..."
 rancher login https://rancher.cd.murex.com/ --token token-vkq9d:wg8gtt4gbgtk7nzfhlj4gs87dn4w2hxhd9qmcb9fmnqkllgx57792r --context $cluster_id:$project_id
+app_version=$(rancher app | grep $team-sonarqube | awk '{print $6}')
 
 echo "Removing ingress ..."
-rancher app upgrade --values myvals.yaml $team-sonarqube 0.1.0 --set ingress.enabled='false' --set sonarqube.image.tag="$current_version"
+rancher app upgrade --values myvals.yaml $team-sonarqube $app_version --set ingress.enabled='false' --set sonarqube.image.tag="$current_version"
 
 echo "Backing up database ..."
 kubectl -n sonarqube exec $POD -c sonardb -- bash -c "pg_dump -U sonar sonar > /var/lib/postgresql/data/sonar-backup.sql"
@@ -26,7 +26,7 @@ echo "Waiting for 5 seconds..."
 sleep 5s
 
 echo "Deploying the upgrade ..."
-rancher app upgrade --values myvals.yaml $team-sonarqube 0.1.0 --set ingress.enabled='true' --set sonarqube.image.tag="$upgrade_to"
+rancher app upgrade --values myvals.yaml $team-sonarqube $app_version --set ingress.enabled='true' --set sonarqube.image.tag="$upgrade_to"
 
 echo "Initiating playbook ..."
 ansible-playbook upgrade.yaml --extra-vars "web_context=$WEBCONTEXT hostname=$HOSTNAME"
